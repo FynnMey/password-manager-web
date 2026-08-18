0bde1eed7c52f7f81cd8f3ddf86fc37da391a867
fynnm <fynnmeyer711@gmail.com>
2026-08-18 15:51:35 +0200

    initial

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace password_manager_web;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace password_manager_web;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
